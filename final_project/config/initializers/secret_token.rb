f1979aba90609bda75dfd60420524f55846495ac
bochie <mboch94@gmail.com>
2014-03-24 08:40:27 -0500

    Added the rails folder

---
+++ final_project/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FinalProject::Application.config.secret_key_base = '2cd7e5af7f9803aaa37232b835b4957f42dad3c09f4a767841bc871ba694a0f4d44c7b9313938146a1f101149f8035a3481c582e65caf867cf38fe806496b476'
